082248a8e539f6d3f69c153218c8797d35a615b5
Jorge Diego daniel Azpeitia <jorgediegodanielazpeitia@MacBook-Pro-de-Jorge.local>
2024-07-23 18:26:00 -0300

    playing with useRef hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+// App.js
+ 
+// Importing useRef
+import { useRef } from "react";
+ 
+const App = () => {
+    //creating a ref
+    const ref = useRef();
+    // Styling the element
+    const handleClick = () => {
+        ref.current.style.backgroundColor = "black";
+        ref.current.style.padding = "3rem";
+        ref.current.style.color = "white";
+        ref.current.style.width = "150px";
+        ref.current.style.height = "150px";
+        ref.current.style.margin = "50px";
+        ref.current.style.borderRadius = "10px";
+    };
+ 
+    return (
+        <div>
+            <h2>Welcome to geeksforgeeks</h2>
+            <button onClick={handleClick}>Enable dark mode</button>
+ 
+            <br />
+            <br />
+ 
+            {/* Passing the ref to the DOM element , 
+                we wish to style */}
+            <div ref={ref}>
+                Article on styling an element using useRef hook in React
+            </div>
+        </div>
+    );
+};
+ 
 export default App;
